5549f96712da8f8d14d380eaf51953dcf1ee9860
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-03-13 12:13:36 -0700

    fix 404 entry point routing

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
+Route::get('/{any?}', function () {
     return file_get_contents(public_path('frontend.html'));
-})->where('any', '^(?!api|sanctum).*$');
+})->where('any', '.*');

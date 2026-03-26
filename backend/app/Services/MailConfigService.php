20fa51266a915bc8592aa86437ab1c94a0915e6d
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-03-26 04:45:39 -0700

    update email service

--- backend/app/Services/MailConfigService.php
+++ backend/app/Services/MailConfigService.php
@@ -26,14 +26,14 @@ class MailConfigService
             }
 
             $config = [
-                'mail.default' => $settingsMap['mail_mailer'] ?? config('mail.default'),
-                'mail.mailers.smtp.host' => $settingsMap['mail_host'] ?? config('mail.mailers.smtp.host'),
-                'mail.mailers.smtp.port' => $settingsMap['mail_port'] ?? config('mail.mailers.smtp.port'),
-                'mail.mailers.smtp.encryption' => $settingsMap['mail_encryption'] ?? config('mail.mailers.smtp.encryption'),
-                'mail.mailers.smtp.username' => $settingsMap['mail_username'] ?? config('mail.mailers.smtp.username'),
-                'mail.mailers.smtp.password' => $settingsMap['mail_password'] ?? config('mail.mailers.smtp.password'),
-                'mail.from.address' => $settingsMap['mail_from_address'] ?? config('mail.from.address'),
-                'mail.from.name' => $settingsMap['mail_from_name'] ?? config('mail.from.name'),
+                'mail.default' => !empty($settingsMap['mail_mailer']) ? $settingsMap['mail_mailer'] : config('mail.default'),
+                'mail.mailers.smtp.host' => !empty($settingsMap['mail_host']) ? $settingsMap['mail_host'] : config('mail.mailers.smtp.host'),
+                'mail.mailers.smtp.port' => !empty($settingsMap['mail_port']) ? $settingsMap['mail_port'] : config('mail.mailers.smtp.port'),
+                'mail.mailers.smtp.encryption' => !empty($settingsMap['mail_encryption']) ? $settingsMap['mail_encryption'] : config('mail.mailers.smtp.encryption'),
+                'mail.mailers.smtp.username' => !empty($settingsMap['mail_username']) ? $settingsMap['mail_username'] : config('mail.mailers.smtp.username'),
+                'mail.mailers.smtp.password' => !empty($settingsMap['mail_password']) ? $settingsMap['mail_password'] : config('mail.mailers.smtp.password'),
+                'mail.from.address' => !empty($settingsMap['mail_from_address']) ? $settingsMap['mail_from_address'] : config('mail.from.address'),
+                'mail.from.name' => !empty($settingsMap['mail_from_name']) ? $settingsMap['mail_from_name'] : config('mail.from.name'),
             ];
 
             config($config);
